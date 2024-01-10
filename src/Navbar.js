2f937425d49092d151386be825786c617bbd2dcd
stevendev97 <x.limsteven@gmail.com>
2024-01-10 07:49:53 -0600

    init

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+
+    return (
+        <div className='navbar'>
+            <p>Home</p>
+            <p>Login</p>
+        </div>
+    )
+}
+
