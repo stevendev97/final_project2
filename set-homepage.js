3c3268d0ca37caeec773996ebbac8215c20fffe7
stevendev97 <x.limsteven@gmail.com>
2024-01-15 08:43:46 -0500

    edited docs

--- set-homepage.js
+++ set-homepage.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const packageJson = require('./package.json');
 
 const setHomepage = () => {
-    packageJson.homepage = "https://stevendev97.github.io/final_project/";
+    packageJson.homepage = "https://stevendev97.github.io/final_project2/";
     fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
 };
 
